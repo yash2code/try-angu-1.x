981a04b41cd16c9b96a477a76846e4d42069bf80
yash chaudhary <yash240196@gmail.com>
2018-01-31 22:20:02 +0530

    component

--- js/app/blog-list/blog-list.component.js
+++ js/app/blog-list/blog-list.component.js
@@ -1,8 +1,14 @@
 "use strict";
 
 angular.module('blogList')
-    .controller('BlogListController',function($scope) {
+    .component('blogList', {
 
-        //console.log('helo');
-        $scope.title = 'hi'
-    });
+        template: '<div class=""><h1 class="new-class"> {{title}} </h1><button ng-click="clickme()">click me</button></div>',
+        controller : function($scope) {
+            $scope.title = "hi";
+
+            $scope.clickme = function() {
+              $scope.title = "clicked";
+            };
+        }
+    })
