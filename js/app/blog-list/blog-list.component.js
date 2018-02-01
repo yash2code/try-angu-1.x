9d4f17274217a20196f79361859ac42dab1646b6
yash chaudhary <yash240196@gmail.com>
2018-02-01 08:22:01 +0530

    loop

--- js/app/blog-list/blog-list.component.js
+++ js/app/blog-list/blog-list.component.js
@@ -3,8 +3,20 @@
 angular.module('blogList')
     .component('blogList', {
 
-        template: '<div class=""><h1 class="new-class"> {{title}} </h1><button ng-click="clickme()">click me</button></div>',
-        controller : function($scope) {
+      //  template: '<div class=""><h1 class="new-class"> {{title}} </h1><button ng-click="clickme()">click me</button></div>',
+      templateUrl: '/templates/blog-list.html',  
+      controller : function($scope) {
+
+            var blogItems = [
+
+                    {title:'title',id:1,description:'desc...'},
+                    {title:'title2',id:2,description:'desc...2'},
+                    {title:'title3',id:3,description:'desc...3'},
+                    {title:'title4',id:4,description:'desc...4'}
+            ] 
+
+            $scope.items = blogItems;
+
             $scope.title = "hi";
 
             $scope.clickme = function() {
